0db8a7629267ca8c29069eaff1b9ca975647a4ed
Thiago Santos <thiagop.santos182@gmail.com>
2021-03-25 19:20:47 -0400

    eslint configuration done

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
-app.listen('3333', ()=> {
+app.listen('3333', () => {
   console.log('Server is running 🚀');
 });
