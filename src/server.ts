dee0866e68e22f1cfb0a851c0f5faec80c7df7e4
Thiago Santos <thiagop.santos182@gmail.com>
2021-04-01 19:25:41 -0400

    routes improvement

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 import express from 'express';
 
+import { router } from './routes';
+
 const app = express();
 app.use(express.json());
+app.use(router);
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
-
-app.listen('3333', () => {
-  console.log('Server is running 🚀');
-});
+app.listen('3333', () => console.log('Server is running 🚀'));
