fc79e7ceb6c9e18c9935d7f5f766588d4b7987d0
Thiago Santos <thiagop.santos182@gmail.com>
2022-05-14 08:36:43 -0400

    Adding create and find all of categories

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-const app = express();
+import { categoriesRoutes } from './routes/categories.routes';
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World Ignite' });
-});
+const app = express();
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running!'));
