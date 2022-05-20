4aeb6552354a0b5c0bf4fb897350f4497ded8185
Thiago Santos <thiagop.santos182@gmail.com>
2022-05-20 09:53:46 -0400

    feat: use case for specification creation and route organization

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,11 @@
 import { Router } from 'express';
 
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
-specificationsRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateSpecificationService(
-    specificationsRepository,
-  );
-
-  const category = createCategoryService.execute({ name, description });
-
-  return response.status(201).json(category);
-});
+specificationsRoutes.post('/', (request, response) =>
+  createSpecificationController.handle(request, response),
+);
 
 export { specificationsRoutes };
